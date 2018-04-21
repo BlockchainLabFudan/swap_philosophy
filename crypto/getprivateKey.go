6323d5c9ac0459321afa52de66035ff436308c42
xu <302531263@qq.com>
2018-04-21 19:57:19 +0800

    getprivatekey

--- crypto/getprivateKey.go
+++ crypto/getprivateKey.go
@@ -2,17 +2,22 @@ package crypto
 
 import (
 
-	"io/ioutil"
-	"strings"
+	//"io/ioutil"
+
 	"encoding/hex"
 	"github.com/btcsuite/btcd/btcec"
+	"os"
+	"bufio"
+	"strings"
+
 )
 
-func Readfile(name string)(*btcec.PrivateKey){
-	person1,_:=ioutil.ReadFile("./"+name+".txt")
-	s:=string(person1)
-	lineStr := strings.Split(s,"\n")
-	pb,_ := hex.DecodeString(lineStr[0])
+func Getprivatekey(path string)(*btcec.PrivateKey){
+	file,_:=os.Open(path)
+	reader:=bufio.NewReader(file)
+	str,_:=reader.ReadString('\n')
+	str=strings.Trim(str,"\n")
+	pb,_ := hex.DecodeString(str)
 	pri1,_:=btcec.PrivKeyFromBytes(btcec.S256(),pb)
 	return pri1
 }
