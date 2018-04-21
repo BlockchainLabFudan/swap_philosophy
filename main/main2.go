c1029365bffcdaf70f170c2eaf0d03d81eb4fa27
scottocs <briliasm@gmail.com>
2018-04-21 17:21:50 +0900

    crypto test

--- main/main2.go
+++ main/main2.go
@@ -1,14 +1,14 @@
 package main
 
-import "github.com/scottocs/swap_philosophy/cyb"
+//import "github.com/scottocs/swap_philosophy/cyb"
 import "github.com/scottocs/swap_philosophy/crypto"
 
 
 func main() {
-	initTmpSKForBob()
-	initTmpSKForAlice()
-	sendBTCToAlice()
-	sendCYBToBob()
-	go cyb.Run()
+	//initTmpSKForBob()
+	//initTmpSKForAlice()
+	//sendBTCToAlice()
+	//sendCYBToBob()
+	//go cyb.Run()
 	crypto.TestGenerateSharedSecret()
 }
