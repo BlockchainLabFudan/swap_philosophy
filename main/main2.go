041549ba44f0b42069b83873fa2d0efba6a71e64
scottocs <briliasm@gmail.com>
2018-04-21 13:17:03 +0900

    overflow of 2018-04-21

---
+++ main/main2.go
@@ -0,0 +1,45 @@
+package main
+
+import "github.com/scottocs/swap_philosophy/cyb"
+
+func initTmpSKForBob()  {
+
+}
+func getPKOfBob(){
+
+}
+func getSigOfBob(){
+
+}
+
+func initTmpSKForAlice()  {
+
+}
+func getPKOfAlice(){
+
+}
+func getSigOfAlice(){
+
+}
+
+func sendBTCDepositToAlice()  {
+	
+}
+func sendCYBToBob()  {
+
+}
+func sendBTCToAlice() {
+
+}
+func refundDeposit()  {
+	
+}
+func main() {
+	initTmpSKForBob()
+	initTmpSKForAlice()
+	sendBTCDepositToAlice()
+	sendCYBToBob()
+	sendBTCToAlice()
+	refundDeposit()
+	go cyb.Run()
+}
